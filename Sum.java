22372807b6705d3bd48f79ffac03fc9482fcfa41
PramodiThilakarathne <121012600+PramodiThilakarathne@users.noreply.github.com>
2023-01-02 15:11:11 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -4,6 +4,7 @@ class Sum{
     int b = 8;
     int c =6;
     int d = 10;
-    System.out.println(a+b); //summation
-    }
+    System.out.println(a+b);//summation
+    System.out.println(a+b+c+d);
+}
 }
